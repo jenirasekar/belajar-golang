1ce31e0463b514db2548e51fec14fe50565e1dc3
Jenira Sekar Az-zahra <jenirasekar@gmail.com>
2024-04-27 20:35:04 +0700

    declare variable

--- main.go
+++ main.go
@@ -4,4 +4,11 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello world")
+
+	// declare variable dengan manifest typing
+	var firstName string = "oscar"
+	var lastName string
+	lastName = "isaac"
+
+	fmt.Printf("hello %s %s!\n", firstName, lastName)
 }
